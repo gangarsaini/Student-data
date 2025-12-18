6f1f3a4d61e9fbe6c9f93341c5febc918626938b
Rani <saini12.rani@gmail.com>
2025-12-18 15:38:13 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,152 @@
+const Stuname = document.querySelector('#name');
+const StundentId = document.querySelector('#student-id');
+const StundentClass = document.querySelector('#studentClass');
+const StudentRoll = document.querySelector('#studentrollNo');
+const btn = document.querySelector('#button-id');
+const studentinfo = document.querySelector('.complete-wrapper');
+const tabledata = document.querySelector("#show-data");
+
+function getStudent(){
+const readData = JSON.parse(localStorage.getItem('StudentData')) || [];
+    return readData;
+}
+
+function saveStudent(data){
+    localStorage.setItem("StudentData", JSON.stringify(data));
+}
+
+const hasdata = getStudent();
+
+function toggleTable(hasdata){
+         if(hasdata.length > 0){
+    studentinfo.classList.remove('table-has-data');
+    }
+    else{
+        studentinfo.classList.add('table-has-data');
+    }
+}
+
+
+function ShowError(input, message){
+ const inputData = input.parentElement.querySelector('.errordesign');
+ inputData.innerHTML = message;
+ return inputData;
+}
+
+function clearError(input){
+ const inputData = input.parentElement.querySelector('.errordesign');
+ inputData.innerHTML = "";
+ return inputData;
+}
+
+let editIndex = null;
+
+
+
+function tableCreation(student,index){
+    const createTr = document.createElement('tr');
+    const createTd1 = document.createElement('td');
+    createTd1.innerHTML = student.name;
+    const createTd2 = document.createElement('td');
+    createTd2.innerHTML = student.id;
+     const createTd3 = document.createElement('td');
+    createTd3.innerHTML = student.studentClass;
+     const createTd4 = document.createElement('td');
+    createTd4.innerHTML = student.rollNumber;
+    const createTd5 = document.createElement('td');
+    createTd5.classList.add('designbtn','col')
+    createTd5.innerHTML = "Delete";
+    createTd5.addEventListener('click', function(){
+        const students = getStudent();
+        students.splice(index,1);
+        saveStudent(students);
+        renderTable();
+        toggleTable(students)
+    });
+    const createTd6 = document.createElement('td');
+    createTd6.classList.add('designbtn')
+    createTd6.innerHTML = "Edit";
+    createTd6.addEventListener('click', function(){
+        editIndex = index;
+        Stuname.value = student.name;
+        StundentId.value = student.id;
+        StundentClass.value = student.studentClass;
+        StudentRoll.value = student.rollNumber;
+        btn.innerHTML = 'Update data';
+        
+
+    })
+    createTr.appendChild(createTd1);
+    createTr.appendChild(createTd2);
+    createTr.appendChild(createTd3);
+    createTr.appendChild(createTd4);
+    createTr.appendChild(createTd5);
+    createTr.appendChild(createTd6);
+    tabledata.appendChild(createTr);
+    
+}
+function renderTable(){
+  tabledata.innerHTML = "";
+  const students = getStudent();
+  students.forEach((student,index) => {
+    tableCreation(student,index);
+  });
+}
+
+
+function ShowStudentData(){
+clearError(Stuname);
+clearError(StundentId);
+clearError(StundentClass);
+clearError(StudentRoll);
+if(Stuname.value == "" || 
+StundentId.value == "" ||
+StundentClass.value == "" ||
+StudentRoll.value == "")return;
+if(!/^[A-Za-z ]+$/.test(Stuname.value)){
+    ShowError(Stuname, "Please Enter the letters only");
+    return;
+    }
+    if(!/^[0-9]+$/.test(StundentId.value)){
+        ShowError(StundentId, "Please Enter the Numbers only");
+        return;
+    }
+    if(!/^[0-9a-zA-Z]+$/.test(StundentClass.value)){
+        ShowError(StundentClass, "Please Enter the Numbers only");
+        return;
+    }
+    if(!/^[0-9]+$/.test(StudentRoll.value)){
+        ShowError(StudentRoll, "Please Enter the Numbers only");
+        return;
+    }
+
+    let Studentdetail = {
+    name:Stuname.value,
+    id:StundentId.value,
+    studentClass:StundentClass.value,
+    rollNumber:StudentRoll.value
+    }
+    let students = getStudent();
+  if(editIndex !== null){
+   students[editIndex] = Studentdetail;
+   btn.innerHTML = "Add Data"
+    editIndex = null
+
+  }
+   else{
+     students.push(Studentdetail);
+     toggleTable(students)
+   }
+   saveStudent(students);
+   renderTable()
+   
+  Stuname.value = "";
+  StundentId.value = "";
+  StundentClass.value = "";
+  StudentRoll.value = "";
+
+}
+renderTable()
+toggleTable(hasdata)
+btn.addEventListener('click', ShowStudentData)
+
